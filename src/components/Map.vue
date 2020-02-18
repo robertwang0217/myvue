77c642c24045a9588730c49bf4859fe333e56649
Robert Wang <robertwang0217@gmail.com>
2020-02-19 10:17:50 +1100

    implement mapbox gl js

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,15 +2,17 @@
     <div id="app">
         <b-container class="bv-example-row">
             <b-row>
-            <b-col>1 of 3</b-col>
-            <b-col>2 of 3</b-col>
-            <b-col>3 of 3</b-col>
+                <b-col>
+                    <div id="map" style='width: 100%; height: 650px;'></div>
+                </b-col>
             </b-row>
         </b-container>
     </div>
 </template>
 
 <script>
+import json from "../assets/files/testBlob.json";
+
 export default {
 
     name: "Map",
@@ -19,9 +21,41 @@ export default {
     },
 
     data: () => ({
+        sourceData: json
     }),
 
     mounted() {
+        var map = new this.$mapbox.Map({
+            container: 'map',
+            accessToken: "pk.eyJ1Ijoicm9iZXJ0d2FuZzAyMTciLCJhIjoiY2s2cnFhMmwzMDhkNzNvcWl5cjFpdXc5dCJ9.gAkCQbVUq9lQY76A368odg",
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [151.207383, -33.869959],
+            zoom: 14,
+
+        });
+
+        map.on('load', function() {
+            map.addSource('points', {
+                'type': 'geojson',
+                'data': '/testBlob.json'
+            });
+
+            map.addLayer({
+                'id': 'mylayer',
+                'type': 'symbol',
+                'source': 'points',
+                'layout': {
+                // get the icon name from the source's "icon" property
+                // concatenate the name to get an icon from the style's sprite sheet
+                'icon-image': ['concat', 'harbor', '-15'],
+                // get the title name from the source's "title" property
+                'text-field': ['get', 'title'],
+                'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+                'text-offset': [0, 0.6],
+                'text-anchor': 'top'
+                }
+            });
+        });        
     },
 
     beforeDestroy () {
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 // Install BootstrapVue
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import mapbox from 'mapbox-gl';
+Object.defineProperty(Vue.prototype, '$mapbox', { value: mapbox });
+
 //custom css
 import './custom.scss'
 
