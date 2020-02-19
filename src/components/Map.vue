2e3751d54306f31d402885681283b031c7ab6541
Robert Wang <robertwang0217@gmail.com>
2020-02-19 11:06:29 +1100

    display porject titles on the map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -11,17 +11,17 @@
 </template>
 
 <script>
-import json from "../assets/files/testBlob.json";
+import sourceData from "../assets/files/testBlob.json";
 
 export default {
 
     name: "Map",
 
     props: {
+
     },
 
     data: () => ({
-        sourceData: json
     }),
 
     mounted() {
@@ -29,7 +29,7 @@ export default {
             container: 'map',
             accessToken: "pk.eyJ1Ijoicm9iZXJ0d2FuZzAyMTciLCJhIjoiY2s2cnFhMmwzMDhkNzNvcWl5cjFpdXc5dCJ9.gAkCQbVUq9lQY76A368odg",
             style: 'mapbox://styles/mapbox/streets-v11',
-            center: [151.207383, -33.869959],
+            center: [151.209862, -33.869334],
             zoom: 14,
 
         });
@@ -37,7 +37,7 @@ export default {
         map.on('load', function() {
             map.addSource('points', {
                 'type': 'geojson',
-                'data': '/testBlob.json'
+                'data': sourceData
             });
 
             map.addLayer({
@@ -49,7 +49,7 @@ export default {
                 // concatenate the name to get an icon from the style's sprite sheet
                 'icon-image': ['concat', 'harbor', '-15'],
                 // get the title name from the source's "title" property
-                'text-field': ['get', 'title'],
+                'text-field': ['get', 'Title', ['get', 'project']],
                 'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
                 'text-offset': [0, 0.6],
                 'text-anchor': 'top'
